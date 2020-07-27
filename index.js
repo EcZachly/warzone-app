819760aa0aaad3263a2499c5ff59875733f321ba
Zach Wilson <zachwilson130@gmail.com>
2020-07-27 14:13:37 -0700

    initial warzone app commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const API = require('call-of-duty-api')({ platform: "battle" });
+
+let friends = ['EcZachly', 'Wapanator']
+let EMAIL = process.env.WARZONE_EMAIL;
+let PASSWORD = process.env.WARZONE_PASSWORD;
+
+API.login(EMAIL, PASSWORD).then((response, api)=>{
+    API.MWcombatwzdate('EcZachly', 0,  0, API.platforms.xbl).then((output) => {
+        console.log(output.matches);
+    }).catch((err) => {
+        console.log(err);
+    });
+}).catch(console.log);
+
+
