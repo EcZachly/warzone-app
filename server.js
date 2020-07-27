d5ba68bf1b297257071b2a8d5b5ea771ac3457a6
Zach Wilson <zachwilson130@gmail.com>
2020-07-27 14:53:09 -0700

    add massiveJS connection

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import helmet from 'helmet';
+import {queryGamers} from "./lib/model/gamer";
+
 let app = express()
 app.use(helmet());
 let PORT = process.env.PORT || 3000;
+
+app.get('/api/gamers', (req, res)=>{
+    queryGamers(req.query).then((gamers)=>{
+        res.json(gamers);
+    })
+})
+
 app.get('/', (req, res)=>{
     res.send("HELLO WORLD FOR WARZONE");
 })
