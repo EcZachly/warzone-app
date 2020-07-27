c2fbcfe2dc266dd61e77d62286ca4d52ae26d02d
Zach Wilson <zachwilson130@gmail.com>
2020-07-27 14:29:05 -0700

    making server more secure with helmet package

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import helmet from 'helmet';
 let app = express()
+app.use(helmet());
 let PORT = process.env.PORT || 3000;
 app.get('/', (req, res)=>{
     res.send("HELLO WORLD FOR WARZONE");
