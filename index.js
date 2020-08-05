7ecd4db6635cf9ffd024c6a8e04641aa1dc1a73f
Zach Wilson <zachwilson130@gmail.com>
2020-08-05 12:38:11 -0700

    adding null check

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let friends = ['EcZachly', 'Wapanator']
 let EMAIL = process.env.WARZONE_EMAIL;
 let PASSWORD = process.env.WARZONE_PASSWORD;
 
+if(!EMAIL || !PASSWORD){
+    throw new Exception("WARZONE_EMAIL OR PASSWORD IS NULL");
+}
+
+
 API.login(EMAIL, PASSWORD).then((response, api)=>{
     API.MWcombatwzdate('EcZachly', 0,  0, API.platforms.xbl).then((output) => {
         console.log(output.matches);
