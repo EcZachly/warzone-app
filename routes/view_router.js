0d4c930a8bbe598792ae7a632f5a18d3f3e2c72c
Zach Wilson <zachwilson130@gmail.com>
2020-08-14 14:01:32 -0700

    adding partials and seo optimizations for app

--- routes/view_router.js
+++ routes/view_router.js
@@ -8,8 +8,15 @@ let view_router = express.Router();
 view_router.get('/gamer/:username', (req, res)=>{
     let viewName = 'player_stat_summary';
     let query = {username: req.params.username};
+
     queryView(viewName, query).then((data)=>{
-        res.render('gamer/detail', {gamer: data[0] })
+        let gamer = data[0];
+        let seoMetadata = {
+            title: 'Warzone stats for ' + data[0].username,
+            keywords: ['warzone', 'stats', 'kdr', 'gulag wins'],
+            description: gamer.username + ' KDR: ' + gamer.kdr.toFixed(2) +  ' Gulag Win Rate: ' + gamer.gulag_win_rate.toFixed(2) * 100 + '%'
+        }
+        res.render('gamer/detail', {gamer: data[0], seoMetadata: seoMetadata })
     });
 });
 
@@ -17,8 +24,15 @@ view_router.get('/gamer/:username', (req, res)=>{
 view_router.get('/gamers', (req, res)=>{
     let viewName = 'player_stat_summary';
     let query = {};
+
     queryView(viewName, query).then((data)=>{
-        res.render('gamer/detail', {gamers: data })
+
+        let seoMetadata = {
+            title: 'Warzone Gamers',
+            keywords: ['warzone', 'stats', 'kdr', 'gulag wins'],
+            description: 'Warzone stats for ' + data.length + ' gamers'
+        }
+        res.render('gamer/list', {gamers: data, seoMetadata: seoMetadata})
     });
 });
 
--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import helmet from 'helmet';
-import {queryGamers} from "./lib/model/gamer";
-import {queryView} from './lib/model/analysis';
 import api_router from './routes/api_router';
 import view_router from './routes/view_router';
-let app = express()
+let app = express();
 app.use(helmet());
 let PORT = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+app.set("views", "views")
 app.use(view_router);
 app.use('/api', api_router)
 
 app.get('/', (req, res)=>{
     res.send("HELLO WORLD FOR WARZONE!");
-})
+});
 app.listen(PORT, (req,res)=>{
     console.log("APP is listening on port 3000");
 });
