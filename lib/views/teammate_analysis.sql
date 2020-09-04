90bf0849cdd1ab7fd8e7a43d42405f2efb9b0b7b
Zach Wilson <zachwilson130@gmail.com>
2020-09-03 17:27:42 -0700

    spicing up teammate table

--- lib/views/teammate_analysis.sql
+++ lib/views/teammate_analysis.sql
@@ -14,23 +14,28 @@ WITH source AS (
 ),
      overall AS (
           SELECT
-            gm.query_username                      AS shooting_player,
-            '(overall)' as helping_player,
-                 COUNT(DISTINCT gm.match_id)                                                               AS num_matches,
-       COUNT(1)                                                               AS num_rows,
-       MIN(start_timestamp)                                                                      AS first_game_time,
-       MAX(start_timestamp)                                                                      AS last_game_time,
-       SUM(gm.assists)                                                                        AS total_assists,
-       AVG(gm.assists)                                                                           AS avg_assists,
-       SUM(gm.kills)                                                                       AS num_kills,
-       AVG(gm.score)                                                                             AS avg_score,
+        gm.query_username                      AS shooting_player,
+        '(overall)' as helping_player,
+       COUNT(DISTINCT gm.match_id)                                                               AS num_matches,
+       SUM(gm.kills)                                                                             AS total_kills,
        AVG(gm.kills)                                                                             AS avg_kills,
+       SUM(gm.damage_done)                                                                             AS total_damage_done,
+       AVG(gm.damage_done)                                                                             AS avg_damage_done,
+       SUM(gm.score)                                                                             AS total_score,
+       AVG(gm.score)                                                                             AS avg_score,
+       SUM(gm.deaths)                                                                            AS total_deaths,
        AVG(gm.deaths)                                                                            AS avg_deaths,
        MAX(gm.kills)                                                                             AS max_kills,
        MAX(gm.deaths)                                                                            AS max_deaths,
        AVG(gm.team_placement)                                                                    AS avg_team_placement,
+       SUM(COALESCE(CAST(objective->>'teams_wiped' AS INT), 0))                                                            AS total_teams_wiped,
+       AVG(COALESCE(CAST(objective->>'teams_wiped' AS INT), 0))                                                            AS avg_teams_wiped,
+       SUM(COALESCE(CAST(objective->>'missions_started' AS INT), 0))                                                       AS total_missions_started,
+       AVG(COALESCE(CAST(objective->>'missions_started' AS INT), 0))                                                       AS avg_missions_started,
        CAST(SUM(gm.kills) AS real) / CASE WHEN SUM(gm.deaths) = 0 THEN 1 ELSE SUM(gm.deaths) END AS kdr,
-       CAST(SUM(gm.gulag_kills) AS REAL)/SUM( CASE WHEN gm.gulag_deaths <= 1 THEN gm.gulag_deaths END + gm.gulag_kills) as gulag_win_rate
+       CAST(SUM(gm.gulag_kills) AS REAL)/SUM( CASE WHEN gm.gulag_deaths <= 1 THEN gm.gulag_deaths END + gm.gulag_kills) as gulag_win_rate,
+       MIN(start_timestamp)                                                                      AS first_game_time,
+       MAX(start_timestamp)                                                                      AS last_game_time
     FROM gamer_matches gm
           JOIN matches_augmented m on gm.match_id = m.match_id
          GROUP BY shooting_player
@@ -41,20 +46,25 @@ WITH source AS (
          SELECT shooting_player                                                                     AS shooting_player,
        helping_player                                   AS helping_player,
        COUNT(DISTINCT gm.match_id)                                                               AS num_matches,
-       COUNT(1)                                                               AS num_rows,
-       MIN(start_timestamp)                                                                      AS first_game_time,
-       MAX(start_timestamp)                                                                      AS last_game_time,
-       SUM(gm.assists)                                                                        AS total_assists,
-       AVG(gm.assists)                                                                           AS avg_assists,
-       SUM(gm.kills)                                                                       AS num_kills,
-       AVG(gm.score)                                                                             AS avg_score,
+         SUM(gm.kills)                                                                             AS total_kills,
        AVG(gm.kills)                                                                             AS avg_kills,
+       SUM(gm.damage_done)                                                                             AS total_damage_done,
+       AVG(gm.damage_done)                                                                             AS avg_damage_done,
+       SUM(gm.score)                                                                             AS total_score,
+       AVG(gm.score)                                                                             AS avg_score,
+       SUM(gm.deaths)                                                                            AS total_deaths,
        AVG(gm.deaths)                                                                            AS avg_deaths,
        MAX(gm.kills)                                                                             AS max_kills,
        MAX(gm.deaths)                                                                            AS max_deaths,
        AVG(gm.team_placement)                                                                    AS avg_team_placement,
+       SUM(COALESCE(CAST(objective->>'teams_wiped' AS INT), 0))                                                            AS total_teams_wiped,
+       AVG(COALESCE(CAST(objective->>'teams_wiped' AS INT), 0))                                                            AS avg_teams_wiped,
+       SUM(COALESCE(CAST(objective->>'missions_started' AS INT), 0))                                                       AS total_missions_started,
+       AVG(COALESCE(CAST(objective->>'missions_started' AS INT), 0))                                                       AS avg_missions_started,
        CAST(SUM(gm.kills) AS real) / CASE WHEN SUM(gm.deaths) = 0 THEN 1 ELSE SUM(gm.deaths) END AS kdr,
-        CAST(SUM(gm.gulag_kills) AS REAL)/SUM( CASE WHEN gm.gulag_deaths <= 1 THEN gm.gulag_deaths END + gm.gulag_kills) as gulag_win_rate
+        CAST(SUM(gm.gulag_kills) AS REAL)/SUM( CASE WHEN gm.gulag_deaths <= 1 THEN gm.gulag_deaths END + gm.gulag_kills) as gulag_win_rate,
+      MIN(start_timestamp)                                                                      AS first_game_time,
+       MAX(start_timestamp)                                                                      AS last_game_time
 FROM source gm
 GROUP BY shooting_player, helping_player
 HAVING COUNT(DISTINCT gm.match_id) >= 10
