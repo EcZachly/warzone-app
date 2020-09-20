886c8011c0410ee0fa17d11cd6c08060da74747f
Zach Wilson <zachwilson130@gmail.com>
2020-09-20 16:09:41 -0700

    fixing detail page query when the user has the same name on two platforms

--- routes/view_router.ts
+++ routes/view_router.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
-import { ApiRequest, ApiResponse } from "../lib/middleware/request_object"
-import { handleRender} from './response_handler';
+import {ApiRequest, ApiResponse} from "../lib/middleware/request_object"
+import {handleRender} from './response_handler';
 import * as Bluebird from 'bluebird';
 import * as _ from 'lodash';
 
@@ -15,71 +15,86 @@ import UtilityService from './../lib/services/UtilityService.js';
 
 //===---==--=-=--==---===----===---==--=-=--==---===----//
 view_router.use((req: ApiRequest, res: ApiResponse, next) => {
-	req.startTime = new Date().getTime();
-	next();
+    req.startTime = new Date().getTime();
+    next();
 });
 
 
-view_router.get('/gamer/:platform/:username', async (req:ApiRequest, res:ApiResponse) => {
-	let views = {
-		'player_stat_summary': {query: {username: req.params.username}},
-		'teammate_analysis': {
-			query: {
-				shooting_player: req.params.username,
-				shooting_player_platform: req.params.platform
-			}
-		},
-		'gamer_stats_graded': {query: {username: req.params.username, platform: req.params.platform}},
-		'gamers': {query: {username: req.params.username, platform: req.params.platform}}
-	};
+view_router.get('/gamer/:platform/:username', async (req: ApiRequest, res: ApiResponse) => {
+    let views = {
+        'player_stat_summary': {
+            query: {
+                username: req.params.username,
+                platform: req.params.platform
+            }
+        },
+        'teammate_analysis': {
+            query: {
+                shooting_player: req.params.username,
+                shooting_player_platform: req.params.platform
+            }
+        },
+        'gamer_stats_graded': {
+            query: {
+                username: req.params.username,
+                platform: req.params.platform
+            }
+        },
+        'gamers': {
+            query: {
+                username: req.params.username,
+                platform: req.params.platform
+            }
+        }
+    };
 
-	let promises = Object.keys(views).map(async (key:string) => await queryView(key, views[key].query));
+    let promises = Object.keys(views).map(async (key: string) => await queryView(key, views[key].query));
 
-	Bluebird.all(promises).then(async (arrData) => {
-		Object.keys(views).forEach((key:string, index:number) => views[key].data = arrData[index]);
+    Bluebird.all(promises).then(async (arrData) => {
+        Object.keys(views).forEach((key: string, index: number) => views[key].data = arrData[index]);
 
-		let gamer = sanitizeGamer(views['player_stat_summary']['data'][0]);
-		let teammates = sanitizeTeammates(views['teammate_analysis']['data']);
-		let grades = views['gamer_stats_graded']['data'][0];
+        let gamer = sanitizeGamer(views['player_stat_summary']['data'][0]);
+        let teammates = sanitizeTeammates(views['teammate_analysis']['data']);
+        let grades = views['gamer_stats_graded']['data'][0];
 
-		let gamerData = views['gamers']['data'][0];
-		let gamerPromise = Bluebird.resolve(gamerData);
+        let gamerData = views['gamers']['data'][0];
+        let gamerPromise = Bluebird.resolve(gamerData);
 
-		if (!gamerData.needs_update) {
-			gamerData.needs_update = true;
-			gamerPromise = updateGamer({username: gamerData.username, platform: gamerData.platform}, gamerData);
-		}
+        if (!gamerData.needs_update) {
+            gamerData.needs_update = true;
+            gamerPromise = updateGamer({username: gamerData.username, platform: gamerData.platform}, gamerData);
+        }
 
-		await gamerPromise;
+        await gamerPromise;
 
-		let titleKeys = Object.keys(teammates[0]).filter((key) => !FILTER_KEYS.includes(key)).map(columnToDisplayName);
+        let titleKeys = Object.keys(teammates[0]).filter((key) => !FILTER_KEYS.includes(key)).map(columnToDisplayName);
 
-		let seoMetadata = {
-			title: 'Warzone stats for ' + gamer.username,
-			keywords: ['warzone', 'stats', 'kdr', 'gulag wins'],
-			description: 'KDR: ' + gamer.kdr + ' Gulag Win Rate: ' + gamer.gulag_win_rate
-		};
+        let seoMetadata = {
+            title: 'Warzone stats for ' + gamer.username,
+            keywords: ['warzone', 'stats', 'kdr', 'gulag wins'],
+            description: 'KDR: ' + gamer.kdr + ' Gulag Win Rate: ' + gamer.gulag_win_rate
+        };
 
-		handleRender(req, res, 'gamer/detail', {
-			gamer: gamer,
-			grades: grades,
-			titleKeys: titleKeys,
-			teammateData: teammates,
-			seoMetadata: seoMetadata,
-			filterKeys: FILTER_KEYS
-		})
-	});
+        handleRender(req, res, 'gamer/detail', {
+            gamer: gamer,
+            grades: grades,
+            titleKeys: titleKeys,
+            teammateData: teammates,
+            seoMetadata: seoMetadata,
+            filterKeys: FILTER_KEYS
+        })
+    });
 });
 
 view_router.get('/gamers', recaptcha.middleware.render, async (req: ApiRequest, res: ApiResponse) => {
     let viewName: string = 'player_stat_summary';
     let queryParams = req.query;
 
-	let submittedUsername: string = (queryParams.submittedUsername || "").toString();
-	let submissionError: string = (queryParams.submissionError || "").toString();
+    let submittedUsername: string = (queryParams.submittedUsername || "").toString();
+    let submissionError: string = (queryParams.submissionError || "").toString();
 
-	let rawGamerList = await queryView(viewName);
-	let sanitizedGamers = rawGamerList.map(sanitizeGamer);
+    let rawGamerList = await queryView(viewName);
+    let sanitizedGamers = rawGamerList.map(sanitizeGamer);
     let seoMetadata = {
         title: 'Warzone Gamers',
         keywords: ['warzone', 'stats', 'kdr', 'gulag wins'],
@@ -100,33 +115,31 @@ view_router.get('/gamers', recaptcha.middleware.render, async (req: ApiRequest, 
 //PRIVATE METHODS
 
 
-function columnToDisplayName(column:string) {
-	return column.split('_').map(_.capitalize).join(' ');
+function columnToDisplayName(column: string) {
+    return column.split('_').map(_.capitalize).join(' ');
 }
 
 
-
 function sanitizeGamer(gamer) {
-	gamer = UtilityService.validateItem(gamer, 'object', {});
-	gamer.gulag_win_rate = (UtilityService.validateItem(gamer.gulag_win_rate, 'number', 0).toFixed(4) * 100).toFixed(2) + '%';
-	gamer.kdr = (UtilityService.validateItem(gamer.kdr, 'number', 0).toFixed(4)).toString();
-	gamer.aliases = UtilityService.validateItem(gamer.aliases, 'array', []);
+    gamer = UtilityService.validateItem(gamer, 'object', {});
+    gamer.gulag_win_rate = (UtilityService.validateItem(gamer.gulag_win_rate, 'number', 0).toFixed(4) * 100).toFixed(2) + '%';
+    gamer.kdr = (UtilityService.validateItem(gamer.kdr, 'number', 0).toFixed(4)).toString();
+    gamer.aliases = UtilityService.validateItem(gamer.aliases, 'array', []);
 
-	return gamer;
+    return gamer;
 }
 
 
 function sanitizeTeammates(teammates) {
-	return teammates.map((teammate) => {
-		teammate = sanitizeGamer(teammate);
-		teammate.helping_player = {
-			name: teammate.helping_player,
-			platform: teammate.helping_player_platform
-		};
-		return teammate;
-	});
+    return teammates.map((teammate) => {
+        teammate = sanitizeGamer(teammate);
+        teammate.helping_player = {
+            name: teammate.helping_player,
+            platform: teammate.helping_player_platform
+        };
+        return teammate;
+    });
 }
-
 
 
 export default view_router;
