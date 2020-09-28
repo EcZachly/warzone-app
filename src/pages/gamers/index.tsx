5c584be9fc585526bd8039e0fad2b8dcff9239d5
Fjaxyu <wbraithwaite@me.com>
2020-09-28 17:34:02 -0600

    started on the new user creation system

--- src/pages/gamers/index.tsx
+++ src/pages/gamers/index.tsx
@@ -1,25 +1,74 @@
 import {GetServerSideProps} from 'next'
-import GamerCard from '../../components/gamer/GamerCard';
+import {useState} from "react";
 
-import {Container, Main} from './../../components/SimpleComponents';
+import GamerCard from '../../components/gamer/GamerCard';
+import {Container, Header, LineBreak, Button, Box, Main} from './../../components/SimpleComponents';
 import {Page, Navbar} from './../../components/AppComponents';
+import {SidebarCompanion, Input, Sidebar} from '../../components/SmartComponents';
+
+const CONFIG = {
+    PLATFORM_OPTIONS: [
+        {text: 'test', value: 'wow'},
+        {text: 'test 2', value: 'wow 2 '},
+    ]
+}
 
 //===---==--=-=--==---===----===---==--=-=--==---===----//
 
 
 export default function Gamers({gamers}) {
+    const [newUserValue, setNewUserValue] = useState('');
+    const [newUserPlatform, setNewUserPlatform] = useState(CONFIG.PLATFORM_OPTIONS);
+
     return (
         <Page title={'Gamers'}>
             <Navbar/>
 
-            <Container>
-                <Main>
-                    {gamers.map((gamer) => <GamerCard key={gamer.username + '-' + gamer.platform} gamer={gamer}/>)}
-                </Main>
-            </Container>
+            <Main>
+                <Container mode={'sidebar'} size={'lg'}>
+                    <Sidebar>
+                        <Header>
+                            All Gamers
+                        </Header>
+
+                        <LineBreak/>
+
+                        <Box>
+                            <Header size={'sm'}>Add Gamer</Header>
+                            <Input label={'Username'}
+                                   type={'text'}
+                                   value={newUserValue}
+                                   onChange={setNewUserValue}
+                                   placeholder={'Username'}/>
+                            <Input label={'Platform'}
+                                   type={'select'}
+                                   options={CONFIG.PLATFORM_OPTIONS}
+                                   value={newUserPlatform}
+                                   onChange={setNewUserPlatform}/>
+                            <Button onClick={() => addGamer(newUserValue, newUserPlatform)}>Add Gamer</Button>
+                        </Box>
+                    </Sidebar>
+                    <SidebarCompanion>
+                        {generateGamerListContent(gamers)}
+                    </SidebarCompanion>
+                </Container>
+            </Main>
+
         </Page>
     )
 }
+
+
+function addGamer(newUserValue, newUserPlatform) {
+    console.log(newUserValue)
+    console.log(newUserPlatform)
+}
+
+
+function generateGamerListContent(gamers) {
+    return gamers.map((gamer) => <GamerCard key={gamer.username + '-' + gamer.platform} gamer={gamer}/>);
+}
+
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     let rawGamerList = await fetch(process.env.HOSTNAME + '/api/gamers');
