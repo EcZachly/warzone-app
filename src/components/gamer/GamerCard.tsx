07d42ec8d30182a23c000a11d4635949e986cac7
Fjaxyu <wbraithwaite@me.com>
2020-11-25 09:58:12 -0700

    fix to stop heat score from showing for users with less than 0.1 delta

--- src/components/gamer/GamerCard.tsx
+++ src/components/gamer/GamerCard.tsx
@@ -42,34 +42,37 @@ export default function GamerCard({gamer, classDescriptions}: GamerCardProps) {
     let gamesPlayed = (Number(gamer.total_kills) / Number(gamer.avg_kills)).toFixed(0);
 
     let heatFlames = <div/>;
-
+    
     if (gamer.heat_rating > 0) {
-        let cnt = 0;
-        let array = [];
-
-        while (cnt < gamer.heat_rating) {
-            array.push(<Image className={'heat-indicator'}
-                              style={{width: '16px', height: '18px', opacity: (52 + (cnt * 12)) + '%'}}
-                              src={'/assets/images/icons/heat-sm.png'}
-                              alt={'number of flames means this player is on a hot streak'}/>);
-            cnt = cnt + 1;
-        }
-
         let delta = (gamer.last_10_rolling_average_kdr / gamer.last_100_rolling_average_kdr - 1) * 100;
 
-        let viewDelta = delta.toFixed(1) + '%';
+        if (delta >= 0.1) {
+            let cnt = 0;
+            let array = [];
 
-        heatFlames = (
-            <Box className={'heat-container'}>
-                <Box className={'heat-scores'}>
-                    {array}
+            while (cnt < gamer.heat_rating) {
+                array.push(<Image className={'heat-indicator'}
+                                  style={{width: '16px', height: '18px', opacity: (52 + (cnt * 12)) + '%'}}
+                                  src={'/assets/images/icons/heat-sm.png'}
+                                  alt={'number of flames means this player is on a hot streak'}/>);
+                cnt = cnt + 1;
+            }
+
+            let viewDelta = delta.toFixed(1) + '%';
+
+            heatFlames = (
+                <Box className={'heat-container'}>
+                    <Box className={'heat-scores'}>
+                        {array}
+                    </Box>
+
+                    <Small className={'details'}>
+                        KDR <Text className={'percentage'}>+{viewDelta}</Text> better than usual
+                    </Small>
                 </Box>
+            );
+        }
 
-                <Small className={'details'}>
-                    KDR <Text className={'percentage'}>+{viewDelta}</Text> better than usual
-                </Small>
-            </Box>
-        );
     }
 
     const showHeatScore = gamer.heat_rating > 0;
