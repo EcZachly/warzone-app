f7cd517468c8ca2d45de30546602f35e227bc8a6
Zach Wilson <zachwilson130@gmail.com>
2020-12-11 12:35:11 -0800

    debugging heroku middleware issue

--- lib/model/events.ts
+++ lib/model/events.ts
@@ -2,6 +2,5 @@ import {insertIntoDatabase} from '../etl/utils';
 import {SITE_EVENTS} from '../constants';
 
 export async function createEvent(event) {
-    console.log(event);
     return insertIntoDatabase(event, SITE_EVENTS);
 }
