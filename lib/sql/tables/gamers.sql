62d54a43e17f0fc401b7b5041e2dc2720af04040
Fjaxyu <wbraithwaite@me.com>
2021-02-28 07:43:22 -0700

    updated the gamer sql table to include no_permission flag

--- lib/sql/tables/gamers.sql
+++ lib/sql/tables/gamers.sql
@@ -4,6 +4,7 @@ CREATE TABLE warzone.gamers
 	platform TEXT,
 	priority_number INTEGER,
 	needs_backfill BOOLEAN,
+	no_permission BOOLEAN,
 	needs_update BOOLEAN,
 	uno_id NUMERIC(38, 0),
 	PRIMARY KEY (username, platform)
